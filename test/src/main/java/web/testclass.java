6be4469cdea18a1fa18099f5e721a5fe88cc683a
AuroraHeo <youjin2586@gmail.com>
2025-01-17 17:13:16 +0900

    final

--- test/src/main/java/web/testclass.java
+++ test/src/main/java/web/testclass.java
@@ -5,7 +5,7 @@ public class testclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("허허허 나는 이수빈~ 집갈래");
-		
+		System.out.println("집갈라");
 
 	}
 
