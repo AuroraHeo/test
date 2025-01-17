e2878e0bd728fd9af1437fe26cc042dde5374973
AuroraHeo <youjin2586@gmail.com>
2025-01-17 16:40:47 +0900

    added by heo

--- test/src/main/java/web/testclass.java
+++ test/src/main/java/web/testclass.java
@@ -4,7 +4,7 @@ public class testclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("하하하");
+		System.out.println("허허허 나는 허유진~ 집갈래");
 
 	}
 
