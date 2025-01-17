19e0cc52d5e13c6d6c044653b037a5ed6588f049
AuroraHeo <youjin2586@gmail.com>
2025-01-17 17:12:12 +0900

    subin

--- test/src/main/java/web/testclass.java
+++ test/src/main/java/web/testclass.java
@@ -4,9 +4,8 @@ public class testclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("허허허 나는 허유진~ 집갈래");
-		System.out.println("난 푸쉬할게~~");
-		System.out.println("하하");
+		System.out.println("허허허 나는 이수빈~ 집갈래");
+		
 
 	}
 
